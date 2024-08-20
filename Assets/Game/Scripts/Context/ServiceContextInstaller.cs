23b0e782bfe60163d445b16c629e6856967936fb
Ykisl <yakhs2014@mail.ru>
2024-08-20 18:13:45 +0700

    Initialize project

---
+++ Assets/Game/Scripts/Context/ServiceContextInstaller.cs
@@ -0,0 +1,13 @@
+using Unity;
+using Zenject;
+
+namespace Game.Context
+{
+    public class ServiceContextInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            base.InstallBindings();
+        }
+    }
+}
